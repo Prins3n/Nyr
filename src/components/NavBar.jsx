f01edd368f317a80a74770856b03d6e5c4cf0825
Prins3n <stamaica@gmail.com>
2023-05-21 09:43:15 +0200

    lagt til scrolltotop på linkene

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,11 +12,19 @@ export const NavBar = () => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+  const handleClick = () => {
+    toggleMenu();
+    scrollToTop();
+  };
+
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-[#e9ebea]`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2">
+        <Link to="/" className="flex items-center gap-2" onClick={scrollToTop}>
           <img src={logo} alt="logo" className="w-18 h-12 cursor-pointer object-contain" />
         </Link>
         <div className="sm:hidden">
@@ -29,24 +37,24 @@ export const NavBar = () => {
           </button>
         </div>
         <ul className="hidden sm:flex flex-row gap-10">
-          <CustomLink to="/">Forside</CustomLink>
-          <CustomLink to="/om">Om Nýr</CustomLink>
-          <CustomLink to="/is">Nýr Is</CustomLink>
-          <CustomLink to="/grondalengaard">Grøndalen Gård</CustomLink>
-          <CustomLink to="/velferd">Dyrevelferd</CustomLink>
-          <CustomLink to="/oppskrifter">Oppskrifter</CustomLink>
-          <CustomLink to="/kontakt">Kontakt Oss</CustomLink>
+          <CustomLink to="/" onClick={scrollToTop}>Forside</CustomLink>
+          <CustomLink to="/om" onClick={scrollToTop}>Om Nýr</CustomLink>
+          <CustomLink to="/is" onClick={scrollToTop}>Nýr Is</CustomLink>
+          <CustomLink to="/grondalengaard" onClick={scrollToTop}>Grøndalen Gård</CustomLink>
+          <CustomLink to="/velferd" onClick={scrollToTop}>Dyrevelferd</CustomLink>
+          <CustomLink to="/oppskrifter" onClick={scrollToTop}>Oppskrifter</CustomLink>
+          <CustomLink to="/kontakt" onClick={scrollToTop}>Kontakt Oss</CustomLink>
         </ul>
       </div>
       <div className={`sm:hidden absolute top-0 right-0 w-full h-screen bg-[#e9ebea] ${isMenuOpen ? '' : 'hidden'}`}>
         <ul className="flex flex-col items-center mt-20">
-          <CustomLink onClick={toggleMenu} to="/">Forside</CustomLink>
-          <CustomLink onClick={toggleMenu} to="/om">Om Nýr</CustomLink>
-          <CustomLink onClick={toggleMenu} to="/is">Nýr Is</CustomLink>
-          <CustomLink onClick={toggleMenu} to="/grondalengaard">Grøndalen Gård</CustomLink>
-          <CustomLink onClick={toggleMenu} to="/velferd">Dyrevelferd</CustomLink>
-          <CustomLink onClick={toggleMenu} to="/oppskrifter">Oppskrifter</CustomLink>
-          <CustomLink onClick={toggleMenu} to="/kontakt">Kontakt Oss</CustomLink>
+          <CustomLink onClick={handleClick} to="/" >Forside</CustomLink>
+          <CustomLink onClick={handleClick} to="/om">Om Nýr</CustomLink>
+          <CustomLink onClick={handleClick} to="/is">Nýr Is</CustomLink>
+          <CustomLink onClick={handleClick} to="/grondalengaard">Grøndalen Gård</CustomLink>
+          <CustomLink onClick={handleClick} to="/velferd">Dyrevelferd</CustomLink>
+          <CustomLink onClick={handleClick} to="/oppskrifter">Oppskrifter</CustomLink>
+          <CustomLink onClick={handleClick} to="/kontakt">Kontakt Oss</CustomLink>
         </ul>
       </div>
     </nav>
@@ -60,11 +68,16 @@ function CustomLink({ to, children, ...props }) {
     color: '#003b1d',
     fontFamily: 'Arial',
     fontStyle: 'Bold'
-  }
+  };
+  const activeStyle = {
+    color: '#b2d587', 
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
+  };
 
   return (
     <li className={isActive === to ? "active" : ""}>
-      <Link to={to} {...props} style={style}>
+      <Link to={to} {...props} style={isActive === to ? activeStyle : style}>
         {children}
       </Link>
     </li>
